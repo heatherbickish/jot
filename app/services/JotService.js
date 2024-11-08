7e6a4aac4b468ed0ffe3cb782cc5fdd54edc7f7e
heather bickish <hbickish@gmail.com>
2024-11-08 16:43:25 -0700

    created save and load functions for local storage

--- app/services/JotService.js
+++ app/services/JotService.js
@@ -1,5 +1,6 @@
 import { AppState } from "../AppState.js";
 import { Jot } from "../models/Jot.js";
+import { loadState, saveState } from "../utils/Store.js";
 
 class JotSevice {
   selectActiveJotList(jotId) {
@@ -12,10 +13,17 @@ class JotSevice {
     AppState.jots.push(createdJot)
     // AppState.activeJot = ''
     console.log(AppState.jots);
+    this.saveJot()
   }
 
 
+  saveJot() {
+    saveState('jots', AppState.jots)
+  }
 
+  loadJot() {
+    AppState.jots = loadState('jots', [Jot])
+  }
 
 }
 
